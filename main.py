34efb6f7b85ad0f40b0a021f3ed3a2f2fffe2a5e
Joseph-R-Jackson <washad.joseph@gmail.com>
2021-01-28 21:53:55 -0700

    makes a new with information I need from database

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import bs4 as bs
 import pickle
 import requests
 import datetime as dt
+import pandas as pd
 import pandas_datareader.data as web
+
 
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -36,7 +38,7 @@ def get_data_from_yahoo(reload_sp500=False):
         os.makedirs('stock_dfs')
 
     start = dt.datetime(2000, 1, 1)
-    end = dt.datetime(2021, 1, 27)
+    end = dt.datetime(2021, 1, 28)
 
     for ticker in tickers:
         print(ticker)
@@ -46,4 +48,30 @@ def get_data_from_yahoo(reload_sp500=False):
         else:
             print('Already have {}'.format(ticker))
 
-get_data_from_yahoo()
+
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+        main_df = pd.DataFrame()
+
+        for count, ticker in enumerate(tickers):
+            df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+            df.set_index('Date', inplace=True)
+
+            df.rename(columns={'Adj Close': ticker}, inplace=True)
+            df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+            if main_df.empty:
+                main_df = df
+            else:
+                main_df = main_df.join(df, how='outer')
+
+            if count % 10 == 0:
+                print(count)
+
+        print(main_df.head())
+        main_df.to_csv('sp500_joined_closes.csv')
+
+
+compile_data()
